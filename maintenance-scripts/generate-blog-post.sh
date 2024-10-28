365da8445d85a236f52a4783cef72b8e0c327da4
Liviu Ionescu <ilg@livius.net>
2024-10-29 00:42:40 +0200

    add generate-blog-post.sh

---
+++ maintenance-scripts/generate-blog-post.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+#
+# This file is part of the xPack project (http://xpack.github.io).
+# Copyright (c) 2024 Liviu Ionescu.  All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/mit/.
+#
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+# set -x
+
+# The script is invoked via the following npm script:
+# "website-generate-commons-and-build": "bash node_modules/@xpack/docusaurus-template-liquid/maintenance-scripts/generate-commons-and-build.sh"
+
+project_folder_path="$(dirname $(dirname $(dirname $(dirname "${script_folder_path}"))))"
+
+source "${script_folder_path}/compute-context.sh"
+
+post_file_path="${website_folder_path}/blog/$(date -u '+%Y-%m-%d')-${npm_package_name}-v$(echo ${npm_package_version} | tr '.' '-')-released.mdx"
+
+echo
+
+if [ -f "${post_file_path}" ]
+then
+    echo "${post_file_path} already there! Remove it to force regenerate."
+    exit 1
+fi
+
+echo "liquidjs -> ${post_file_path}"
+liquidjs --context "${context}" --template "@${website_folder_path}/blog/_templates/blog-post-release-liquid.mdx" > "${post_file_path}"
+
+echo
+echo "${script_name} done"
+
+# Completed successfully.
+exit 0
